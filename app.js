9c0e359cc1312b51f3ef93c335e91f7bd04a7e64
Your New Username <your_new_email@example.com>
2025-05-12 23:39:38 +0530

    Added full feature list to README

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ app.use((err, req, res, next) => {
 const Complaint = require('./models/Complaint');
 
 // Start server
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
